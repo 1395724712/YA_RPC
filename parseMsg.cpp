7c91af1d1d58f6b38446dcf6d99f66e6d99d84c5
lucky7 <1395724712@qq.com>
2021-11-25 00:49:09 -0800

    parseMsg has been almost finish but not tested

--- parseMsg.cpp
+++ parseMsg.cpp
@@ -1,7 +1,12 @@
 #include"parseMsg.hpp"
 
+#include<algorithm>
+#include<iostream>
+#include<assert.h>
+
 parseMsg::parseMsg(function<float(float,float)> sum,function<string(string)> uppercase):
 requestMsg_({}),
+replyMsg_({}),
 leftBraceCount_(0),
 rightBraceCount_(0),
 parseIdx(0),
@@ -9,3 +14,60 @@ sum_(sum),
 uppercase_(uppercase)
 {}
 
+bool parseMsg::getRequestMsg(const string& msg){
+
+#ifdef DEBUG_GETMSG_TEST
+    cout<<msg<<endl;
+#endif
+
+    assert(parseIdx==msg.size());
+    
+    requestMsg_ += msg;
+
+    //统计左右大括号出现的次数，以此记录是否收到完整消息
+    for(;parseIdx<requestMsg_.size();parseIdx++){
+        if(requestMsg_[parseIdx]=='{')
+            leftBraceCount_++;
+        else if(requestMsg_[parseIdx]=='}')
+            rightBraceCount_++;
+    }
+
+    assert(leftBraceCount_>=rightBraceCount_);
+    if(leftBraceCount_==rightBraceCount_)
+        return true;//已收到完整报文，可以应答
+    else
+        return false;//未收到完整报文，不得应答
+}
+
+string parseMsg::getReplyMsg(){
+    assert(leftBraceCount_==rightBraceCount_&&parseIdx==requestMsg_.size());
+
+    if(!replyMsg_.empty())
+        return replyMsg_;
+    
+    //1、解析requestMsg_报文
+    Reader reader;
+    Value requestVal;
+    reader.parse(requestMsg_,requestVal);
+
+    Value replyVal;
+    if(requestVal["function"].asString() == "sum"){
+        float a = requestVal["a"].asFloat();
+        float b = requestVal["b"].asFloat();
+
+        replyVal["result"] = sum_(a,b);
+        replyMsg_ = replyVal.toStyledString();
+    }
+    else if(requestVal["function"].asString() == "uppercase"){
+        string srcStr = requestVal["str"].asString();
+        transform(srcStr.begin(),srcStr.end(),srcStr.begin(),::toupper);
+        replyVal["result"] = srcStr;
+        replyMsg_ = replyVal.toStyledString();
+    }
+    
+#ifdef DEBUG_GETREPLYMSG_TEST
+    cout<<replyMsg_<<endl;
+#endif
+
+    return replyMsg_;
+}
--- parseMsg.hpp
+++ parseMsg.hpp
@@ -6,18 +6,31 @@
 #include<string>
 #include"json/json.h"
 
+#define DEBUG_TEST
+#ifdef DEBUG_TEST
+#define DEBUG_GETMSG_TEST
+#define DEBUG_GETREPLYMSG_TEST
+#endif
+
 using namespace std;
 using namespace Json;
 class parseMsg{
 public:
     parseMsg(function<float(float,float)> sum,function<string(string)> uppercase);
 
-    void getMsg(string msg);
+    //1、获得并解析请求消息
+    bool getRequestMsg(const string& msg);
+
+    //2、返回应答消息
+    string getReplyMsg();
 
 private:
     //1、请求数据
     string requestMsg_;
     
+    //1.1 应答消息
+    string replyMsg_;
+
     //2、左大括号数量
     unsigned int leftBraceCount_;
     //3、右大括号数量
@@ -31,6 +44,6 @@ private:
     //6、远程调用uppercase
     function<string(string)> uppercase_;
     //7、结束连接的远程调用
-}
+};
 
 #endif
